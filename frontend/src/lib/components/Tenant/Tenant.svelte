5d7f63e5273e7b5e561d4cc576e2245f825a6e24
jackdisalvatore <jackp.disalvatore@gmail.com>
2024-04-30 15:16:20 -0500

    displaying tenants
    Signed-off-by: jackdisalvatore <jackp.disalvatore@gmail.com>

---
+++ frontend/src/lib/components/Tenant/Tenant.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let tenant;
+</script>
+
+{tenant.name}
--- frontend/src/routes/tenant/+page.svelte
+++ frontend/src/routes/tenant/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Tenant</h1>
+<script lang="ts">
+	import Tenant from '$lib/components/Tenant/Tenant.svelte';
+	import { useUnityAuthContext } from '$lib/context/UnityAuthContext';
+
+	const unityAuthContext = useUnityAuthContext();
+	const user = unityAuthContext.user;
+</script>
+
+<h1>Tenants</h1>
+
+{#if $user && $user.tenants}
+	<ul>
+		{#each $user.tenants as tenant}
+			<li>
+				<Tenant {tenant} />
+			</li>
+		{/each}
+	</ul>
+{/if}
